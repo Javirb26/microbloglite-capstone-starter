7eed63cdbe699d6704c8c3b59de0a54818753463
Amaris Sanders <146863945+AmarisNichole@users.noreply.github.com>
2024-01-02 09:20:52 -0500

    modified the index.html, profile.css, and the profile.js in the profile folder

---
+++ profile/profile.js
@@ -0,0 +1,11 @@
+window.onload = () => {
+    let logoutButtonEl = document.getElementById("logout");
+    let bioAreaEL = document.getElementById("bio-area");
+    let profileContainerEl = document.getElementById("profile-container");
+    let userPostEl = document.getElementById("user-post");
+    let saveBtnEl = document.getElementById("save-btn");
+    let userDataEl = document.getElementById("user-data-container");
+    let bioTextEl = document.getElementById("bio-text");
+    let postBtnEl = document.getElementById("submit");
+
+}

aafcc92bc2c9453482d0d401507b87485721605e
Amaris Sanders <146863945+AmarisNichole@users.noreply.github.com>
2024-01-02 11:03:56 -0500

    working on the styling of the profile page

--- profile/profile.js
+++ profile/profile.js
@@ -1,11 +1,13 @@
 window.onload = () => {
-    let logoutButtonEl = document.getElementById("logout");
-    let bioAreaEL = document.getElementById("bio-area");
-    let profileContainerEl = document.getElementById("profile-container");
-    let userPostEl = document.getElementById("user-post");
-    let saveBtnEl = document.getElementById("save-btn");
-    let userDataEl = document.getElementById("user-data-container");
+    let logoutBtnEl = document.getElementById("logout");
+    let userDataContainerEl = document.getElementById("user-data-container");
+    let bioAreaEL = document.getElementById("bio-area")
     let bioTextEl = document.getElementById("bio-text");
-    let postBtnEl = document.getElementById("submit");
+    let saveBtnEl = document.getElementById("save-btn");
+    let userPostEl = document.getElementById("user-post");
+    let postAreaEl = document.getElementById("post-area");
+    let submitBtnEL = document.getElementById("submit")
+    let profileContainerEL = document.getElementById("profile-container");
 
+    
 }
